aa3fe2fdba9522791d9e847874375dc7382f3e94
ArdaBaranSuludere <ardasuludere361@gmail.com>
2024-05-29 16:48:11 +0300

    changed fetch structure of the blogs

--- frontend/src/Components/Blogs/blog-container.jsx
+++ frontend/src/Components/Blogs/blog-container.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import BlogBox from './blog-box'; // BlogBox bileşeninin doğru yolunu kullanın
+import { useState, useEffect } from 'react';
 
 const BlogContainer = () => {
+
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:5000/api/blogs')
+            .then(response => response.json())
+            .then(data => setBlogs(data))
+            .catch(error => console.error('Error fetching blogs:', error));
+    }, []);
+
+    
     return (
         <div className="blog-container">
-            <BlogBox />
+            <BlogBox items={blogs}/>
         </div>
     );
 };
--- frontend/src/Components/HomePage/home_page.jsx
+++ frontend/src/Components/HomePage/home_page.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import BlogContainer from '../Blogs/blog-container';
-import BlogBox from '../Blogs/blog-box';
-
 
 const HomePage = () => {
     return (
         <React.Fragment>
             <div className="container">
-                <BlogBox />
+                <BlogContainer />
             </div>
         </React.Fragment>
     );
