995286a557f9f31b5bf38bb2bf03ba4a9ac60dc1
ArdaBaranSuludere <ardasuludere361@gmail.com>
2024-06-03 23:38:48 +0300

    added props

--- frontend/src/Components/MyProfile/col-lg-10.jsx
+++ frontend/src/Components/MyProfile/col-lg-10.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
 import BlogBox from '../Blogs/blog-box';
+import React, { useState, useEffect } from 'react';
 
 const Col10 = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:5000/api/user_blogs', {
+            method: 'GET',
+            credentials: 'include' // include cookies for session
+        })
+        .then(response => response.json())
+        .then(data => setBlogs(data))
+        .catch(error => console.error('Error fetching user blogs:', error));
+    }, []);
+
     return (
         <div className="col-lg-10">
-            <BlogBox />
+            <BlogBox items={blogs} />
         </div>
     );
 };
