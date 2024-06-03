9f80c5f63003ff9ecff61fd5d7fec627272e8558
ArdaBaranSuludere <ardasuludere361@gmail.com>
2024-06-03 23:35:10 +0300

    tryed-proptype

--- frontend/src/Components/Blogs/blog-box.jsx
+++ frontend/src/Components/Blogs/blog-box.jsx
@@ -1,35 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-function BlogBox(props) {
-
-    const itemList = props.items;
-
-    const ListItems = itemList.map(item => 
-        <div key={item.id} className="blog-box mb-3">
-            <img className="blog-img w-100" src="src\assets\images\news-700x435-1.jpg" alt="Blog"/>
-            <div key={item.id} className="cat-date-container">
-                <div className="cat-date">
-                    <a href="#" className="category">{item.category}</a>
-                    <a href="#" className="publish-date">{item.publish_date}</a>
-                </div>
-                <a className="blog-title" href="#">{item.title}</a>
-                <p className="blog-subtitle">{item.subtitle}</p>
-            </div>
-            <div className="author-infos-views">
-                <div className="author-infos">
-                    <img className="author-photo mr-2" src="src\assets\images\user-images\arda.jpg" alt="Author" width="50" height="50"/>
-                    <p className="author-name">DOGAN</p>
-                </div>
-                <div className="views">
-                    <small className="blog-views"><i className="far fa-eye mr-2"></i>{item.views}</small>
-                </div>
-            </div>
-        </div>
-    );
-    
+const BlogBox = ({ items, authors }) => {
     return (
-        <>{ListItems}</>
+        <>
+            {items.map(item => (
+                <div key={item.id} className="blog-box mb-3">
+                    <img className="blog-img w-100" src="src/assets/images/news-700x435-1.jpg" alt="Blog" />
+                    <div key={item.id} className="cat-date-container">
+                        <div className="cat-date">
+                            <a href="#" className="category">{item.category}</a>
+                            <a href="#" className="publish-date">{item.publish_date}</a>
+                        </div>
+                        <a className="blog-title" href="#">{item.title}</a>
+                        <p className="blog-subtitle">{item.subtitle}</p>
+                    </div>
+                    <div className="author-infos-views">
+                        <div className="author-infos">
+                            <img className="author-photo mr-2" src="src/assets/images/user-images/arda.jpg" alt="Author" width="50" height="50" />
+                            <p className="author-name">{authors[item.author_id] ? authors[item.author_id].username : 'Unknown'}</p>
+                        </div>
+                        <div className="views">
+                            <small className="blog-views"><i className="far fa-eye mr-2"></i>{item.views}</small>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </>
     );
 };
+
+// BlogBox.prototype = {
+//     items: PropTypes.shape({
+//         author_id: PropTypes.number.isRequired,
+//         category: PropTypes.string,
+//         content: PropTypes.string,
+//         id: PropTypes.number,
+//         publish_date: PropTypes.date,
+//         reading_time: PropTypes.number,
+//         subtitle: PropTypes.string,
+//         title: PropTypes.string,
+//         views: PropTypes.number
+//       })
+// }
 
 export default BlogBox;
