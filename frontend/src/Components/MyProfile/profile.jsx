7ebe7ab9583b2a930378736fd0f9977d2e8a663e
ArdaBaranSuludere <ardasuludere361@gmail.com>
2024-06-03 23:38:34 +0300

    changes-for-myProfile

--- frontend/src/Components/MyProfile/profile.jsx
+++ frontend/src/Components/MyProfile/profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
         <div className="profile">
             {/* Profil resmi */}
             <div className="profile-image">
-                <img className="author-my-photo mr-2" src="src/assets/images/user-images/emre.jpg" alt="" />
+                <img className="author-my-photo mr-2" src="\src\assets\images\default.jpg" alt="" />
                     {/* <img className="blog-img" src="assets/images/default.jpg" style="border-radius: 0%;" alt="Varsayilan Resim" > */}
                     
                     <form action="{{ url_for('upload_profile_image') }}" method="POST" encType="multipart/form-data">
@@ -13,8 +13,8 @@ const Profile = () => {
                         <button type="submit">Profil Resmi Yükle</button>
                     </form>
 
-                <p>Kullanıcı Adı: arda</p>
-                <p>E-posta: abs@gmail.com</p>
+                <p>Kullanıcı Adı: </p>
+                <p>E-posta: </p>
             </div>
         </div>
     );
