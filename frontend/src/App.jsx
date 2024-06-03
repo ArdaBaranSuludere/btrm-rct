4a5b6b86c27b38fe023b0fa24948ea082d625d22
ArdaBaranSuludere <ardasuludere361@gmail.com>
2024-06-03 23:37:35 +0300

    added logout and changed routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,24 +5,23 @@ import HomePage from './Components/HomePage/home_page';
 import MyProfilePage from './Components/MyProfile/my-profile';
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
-
+import Logout from './Components/Logout/Logout';
 
 
 const App = () => {
 
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(true); // varsayılan olarak kullanıcı giriş yapmış
-  
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(); // Varsayılan olarak kullanıcı giriş yapmış
+  const [isAdmin, setIsAdmin] = useState(); // isAdmin state'ini ekleyin
+
   return (
     <div>
-      <Navbar />
+      <Navbar isLoggedIn={isUserLoggedIn} isAdmin={isAdmin} /> {/* isAdmin değerini Navbar bileşenine iletiliyor */}
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path='myProfile.html' element={<MyProfilePage />} />
-        {/* <Route path="/about" element={<About />} /> */}
-        {/* <Route path="/tickets" element={<Tickets />} /> */}
-        <Route path="/register.html" element={<Register />} />
-        <Route path="/login.html" element={<Login />} />
-        {/* <Route path="/logout" element={<Logout setIsUserLoggedIn={setIsUserLoggedIn} />} /> */}
+        <Route path="/myProfile" element={<MyProfilePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
       </Routes>
     </div>
   );
